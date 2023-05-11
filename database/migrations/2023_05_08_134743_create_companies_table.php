6e1ab8f2fea2b50290b0a73e710f782cd6119ca3
Mohamed190477 <84937256+Mohamed190477@users.noreply.github.com>
2023-05-11 12:14:43 +0300

    Initial commit

---
+++ database/migrations/2023_05_08_134743_create_companies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('company_name');
+            $table->string('contact_person_name');
+            $table->text('company_industry')->nullable();
+            $table->string('contact_person_phone_number');
+            $table->string('email');
+            $table->string('company_address');
+            $table->decimal('longitude', 10, 7);
+            $table->decimal('latitude', 10, 7);
+            $table->enum('company_size', ['Micro', 'Small', 'Mini', 'Large'])->nullable();
+            $table->string('password');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
